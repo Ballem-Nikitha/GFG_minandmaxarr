b5a662c03d5bbe3e6fddbc2e38f59d580ece1b03
Ballem Nikitha <bnikitha9618@gmail.com>
2025-08-18 19:45:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Min and Max in Array/min-and-max-in-array.py
@@ -0,0 +1,8 @@
+#User function Template for python3
+#User function Template for python3
+
+class Solution:
+    def get_min_max(self, arr):
+        max_ele=max(arr)
+        min_ele=min(arr)
+        return min_ele,max_ele
